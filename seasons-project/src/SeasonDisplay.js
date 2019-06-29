d220199cda9b957a95add4cb995cb224d89aef5c
hdmi <c.angorrilla@gmail.com>
2019-06-29 16:52:31 +0200

    Making use of class components

---
+++ seasons-project/src/SeasonDisplay.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+
+  return (
+
+    <div>Hola</div>
+
+  );
+
+};
+
+React.render(<App/>, document.querySelector('#root'));
