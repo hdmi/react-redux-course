98f1cc41048bbcbb21ff020d3377f261a9568a1d
hdmi <c.angorrilla@gmail.com>
2019-07-02 12:46:09 +0200

    Start Pics Project, created SearchBar component

---
+++ pics_project/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import SearchBar from './SearchBar'
+
+class App extends React.Component {
+
+  onSearchSubmit(term) {
+    console.log(`Search: ${term}`);
+  }
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onSubmit={this.onSearchSubmit}/>
+      </div>
+    );
+  }
+
+
+}
+
+export default App;
