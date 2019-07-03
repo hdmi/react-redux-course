248a54baeb6af946ab8e5540bf310f690d079c4a
hdmi <c.angorrilla@gmail.com>
2019-07-03 20:30:52 +0200

    Added calls to Unsplash API for images retrievals

--- pics_project/src/components/App.js
+++ pics_project/src/components/App.js
@@ -1,21 +1,31 @@
 import React from 'react';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
 
 class App extends React.Component {
 
-  onSearchSubmit(term) {
-    console.log(`Search: ${term}`);
+  state = { images: [] };
+
+  onSearchSubmit = async (term) => {
+    const response = await unsplash.get('/search/photos', {
+        params: {
+          query: term
+        }
+      });
+
+      this.setState({images: response.data.results});
+    }
+
+    render() {
+      return (
+        <div className="ui container">
+          <SearchBar onSubmit={this.onSearchSubmit}/>
+          Found {this.state.images.length} images
+        </div>
+      );
+    }
+
+
   }
 
-  render() {
-    return (
-      <div className="ui container">
-        <SearchBar onSubmit={this.onSearchSubmit}/>
-      </div>
-    );
-  }
-
-
-}
-
-export default App;
+  export default App;
