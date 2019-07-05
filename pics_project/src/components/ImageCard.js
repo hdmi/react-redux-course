bfb882e3959f7398da18cee13ddf3beab20ecdbf
hdmi <c.angorrilla@gmail.com>
2019-07-05 16:32:19 +0200

    Added style to ImageList and created component ImageCard

---
+++ pics_project/src/components/ImageCard.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class ImageCard extends React.Component {
+
+
+  render() {
+    const {description, urls} = this.props.image;
+
+    return (
+      <img
+        alt={description}
+        src={urls.regular}
+      />
+    );
+
+  }
+
+}
+
+export default ImageCard;
--- pics_project/src/components/ImageList.js
+++ pics_project/src/components/ImageList.js
@@ -1,16 +1,18 @@
+import './ImageList.css';
 import React from 'react';
+import ImageCard from './ImageCard';
 
 class ImageList extends React.Component {
 
   render () {
-    const images = this. props.images.map(({description, id, urls}) => {
+    const images = this. props.images.map((image) => {
       return (
-        <img alt={description} key={id} src={urls.regular}/>
+        <ImageCard key={image.id} image={image}/>
       );
     });
 
     return (
-      <div>{images}</div>
+      <div className='image-list'>{images}</div>
     );
   }
 
