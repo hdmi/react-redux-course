a2ebdc1392dafaeb2129f630959ee1aac65689be
hdmi <c.angorrilla@gmail.com>
2019-07-06 12:20:04 +0200

    Tiling of ImageList

--- pics_project/src/components/ImageCard.js
+++ pics_project/src/components/ImageCard.js
@@ -2,6 +2,23 @@ import React from 'react';
 
 class ImageCard extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { spans: 0 };
+    this.imageRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener('load', this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+    const spans = Math.ceil(height / 10);
+
+    this.setState({spans});
+  }
+
 
   render() {
     const {description, urls} = this.props.image;
@@ -10,6 +27,8 @@ class ImageCard extends React.Component {
       <img
         alt={description}
         src={urls.regular}
+        ref={this.imageRef}
+        style= {{gridRowEnd: `span ${this.state.spans}`}}
       />
     );
 
