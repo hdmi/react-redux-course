aa7707b86560016a567aba808bb444a0368d0c2e
hdmi <c.angorrilla@gmail.com>
2019-07-08 19:00:19 +0200

    Start Video project

---
+++ videos_project/src/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const YOUTUBE_KEY = ${process.env.REACT_APP_YOUTUBE_API};
+
+
+
+export default axios.create({
+  baseURL: 'https://api.unsplash.com/',
+  headers: {
+    Authorization: `Client-ID ${process.env.REACT_APP_UNSPLASH_API_KEY}`
+  }
+});
---
+++ videos_project/src/components/SearchBar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+  this.state = {term: ''}
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  }
+
+  onChangeInput = e => {
+    this.setState({term: e.target.value})
+  }
+
+  render() {
+    return (
+      <div className="search-bar ui segment">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label>Video Search</label>
+            <input type="text" value={this.state.term} onChange={this.onChangeInput}/>
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SearchBar;
