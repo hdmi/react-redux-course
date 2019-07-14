fa48f3607cd1f609d921a4f128b5091b17f1c49d
hdmi <c.angorrilla@gmail.com>
2019-07-14 16:37:30 +0200

    Songs project: SongList component added

--- songs_project/src/components/App.js
+++ songs_project/src/components/App.js
@@ -1,8 +1,16 @@
 import React from 'react';
+import SongList from './SongList'
+
 
 const App = () => {
     return (
-      <div>songs app</div>
+      <div className="ui container grid">
+        <div className="ui row">
+          <div className="column eight wide">
+            <SongList />
+          </div>
+        </div>
+      </div>
     );
 };
 
