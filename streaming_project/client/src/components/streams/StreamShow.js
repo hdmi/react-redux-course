710c8f8bdc243d2cd6539e39c0f626ef7e167ffd
hdmi <c.angorrilla@gmail.com>
2019-08-02 21:16:49 +0200

    Streaming project: Added RTMP server for streaming

--- streaming_project/client/src/components/streams/StreamShow.js
+++ streaming_project/client/src/components/streams/StreamShow.js
@@ -1,9 +1,44 @@
 import React from 'react';
+import {connect} from 'react-redux';
+import {fetchStream} from '../../actions';
+import flv from 'flv.js';
 
-const StreamShow = () => {
-  return (
-    <div>StreamShow</div>
-  );
+class StreamShow extends React.Component {
+
+  // Setup JSX Reference
+  constructor(props) {
+    super(props);
+    this.videoRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id);
+    flv.createPlayer({
+      type: 'flv',
+      url: ''
+    })
+  }
+
+  render() {
+
+    if(!this.props.stream) {
+      return <div>Loading...</div>
+    }
+      const {title, description} = this.props.stream;
+    return (
+      <div>
+        <video ref={this.videoRefW} style={{width: '100%'}} controls/>
+        <h1>{title}</h1>
+        <h5>{description}</h5>
+      </div>
+    );
+  }
+
 }
 
-export default StreamShow;
+const mapStateToProps = (state, ownProps) => {
+  const id = ownProps.match.params.id;
+  return {stream: state.streamReducer[id]}
+}
+
+export default connect(mapStateToProps, {fetchStream})(StreamShow);
